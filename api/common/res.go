0f24ef4a48ee064a95ebedafe18624165187d504
saibr <errorr0819@gmail.com>
2025-04-16 16:48:09 +0800

    refactor(api): 修改分页结果结构字段名

--- api/common/res.go
+++ api/common/res.go
@@ -16,8 +16,8 @@ type SignRes struct {
 	UserReceive decimal.Decimal        `json:"userReceive"`
 }
 type PaginatedResult[T any] struct {
-	Page      int `json:"page"`      // 当前页
-	PageSize  int `json:"pageSize"`  // 每页记录数
-	TotalPage int `json:"totalPage"` // 总页数
-	Data      []T `json:"data"`      // 当前页数据
+	Page     int `json:"page"`     // 当前页
+	PageSize int `json:"pageSize"` // 每页记录数
+	Total    int `json:"total"`    // 总页数
+	Data     []T `json:"data"`     // 当前页数据
 }
