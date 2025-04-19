57ba65cbfdbd72d29ec5635f87d7b629da99b075
saibr <errorr0819@gmail.com>
2025-04-19 18:13:38 +0800

    修正了查询总数的逻辑错误，并调整了错误情况下的默认值。

--- api/http/controller/memeVault.go
+++ api/http/controller/memeVault.go
@@ -461,17 +461,17 @@ func MemeVaultList(c *gin.Context) {
 	}
 	offset := (req.Page - 1) * req.PageSize
 	var memes []model.MemeVault
+	var totalCount int64
+	if err := query.Count(&totalCount).Error; err != nil {
+		totalCount = 0
+		mylog.Error("MemeVaultList:queryCountError:" + err.Error())
+	}
 	err := query.Order("ID").Limit(req.PageSize).Offset(offset).Find(&memes).Error
 	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 		res.Code = codes.CODE_ERR
 		res.Msg = "Invalid GetMemeVaultList:queryError:" + err.Error()
 		c.JSON(http.StatusOK, res)
 		return
-	}
-	var totalCount int64
-	if err := query.Count(&totalCount).Error; err != nil {
-		totalCount = 200
-		mylog.Error("MemeVaultList:queryCountError:" + err.Error())
 	}
 	var memeVos1 []MemeVaultVo
 	memeVos1 = memeVaultToVo(memes...)
