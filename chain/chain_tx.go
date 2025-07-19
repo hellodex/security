21d2b3e4a1de8037d102df3a150318b57b14b9e6
hellodex <hellodex.io@gmail.com>
2025-07-19 16:20:04 +0800

    1

--- chain/chain_tx.go
+++ chain/chain_tx.go
@@ -353,9 +353,9 @@ func HandleMessage(t *config.ChainConfig, messageStr string, to string, typecode
 				mylog.Errorf("[jito]unparsed data %s %v", tipAdd, err)
 			} else if conf.Tip.Cmp(ZERO) == 1 {
 				// 设置jito费用
-				mylog.Infof("jito小费 %s", conf.Tip.String())
+				//mylog.Infof("jito小费 %s", conf.Tip.String())
 				//_, _ = SimulateTransaction(rpcList, tx, conf)
-				AddInstruction(rpcList, tx, "3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT", conf.Tip, wg.Wallet)
+				//AddInstruction(rpcList, tx, "3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT", conf.Tip, wg.Wallet)
 				//设置优先费
 				//tx.Message.Instructions = appendUnitPrice(conf, tx)
 			}
