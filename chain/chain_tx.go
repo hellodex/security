7fd466b82219d48f576cd4b1c95e71abb97a1744
hellodex <hellodex.io@gmail.com>
2025-07-19 13:44:33 +0800

    1

--- chain/chain_tx.go
+++ chain/chain_tx.go
@@ -354,12 +354,15 @@ func HandleMessage(t *config.ChainConfig, messageStr string, to string, typecode
 			} else if conf.Tip.Cmp(ZERO) == 1 {
 				// 设置jito费用
 				mylog.Infof("jito小费 %s", conf.Tip.String())
-				_, _ = SimulateTransaction(rpcList, tx, conf)
+				//_, _ = SimulateTransaction(rpcList, tx, conf)
 				//AddInstruction(tx, "3AVi9Tg9Uo68tJfuvoKvqKNWKkC5wPdSSdeBnizKZ6jT", conf.Tip, wg.Wallet)
 				//设置优先费
-				tx.Message.Instructions = appendUnitPrice(conf, tx)
+				//tx.Message.Instructions = appendUnitPrice(conf, tx)
 			}
 		}
+		//设置优先费加速上链
+		_, _ = SimulateTransaction(rpcList, tx, conf)
+		tx.Message.Instructions = appendUnitPrice(conf, tx)
 
 		// 记录获取最新区块哈希的开始时间。
 		timeStart := time.Now().UnixMilli()
